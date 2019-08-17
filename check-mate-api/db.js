b102d53b141573f5fadb95b1e066621d96f5a8d0
Georgi Georgiev <georgiwess@gmail.com>
2019-08-17 10:41:38 +0300

    Add helper methods to db.js

--- check-mate-api/db.js
+++ check-mate-api/db.js
@@ -42,3 +42,21 @@ interface Bill {
 const db = {};
 
 const colors = ['#DB7F8E', '#9392B7', '#9DA3A4', '#FFDBDA', '#D5C5C8'];
+
+const generateNewId = () => +new Date();
+
+const getNewColorForBillId = (id) => {
+  if (typeof db[id] === 'undefined') {
+    return colors[0];
+  }
+
+  return colors.find((color) => !db[id].users.some((user) => user.color === color));
+};
+
+const getBill = (id) => db[id];
+
+module.exports = {
+  generateNewId,
+  getNewColorForBillId,
+  getBill
+};
