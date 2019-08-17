8f4a4112bbe8527d47b24202912cb145cf479380
SvetozarMateev <vidic9898@gmail.com>
2019-08-17 16:05:41 +0300

    WIP on last page

--- check-mate-app/app/src/main/java/com/example/checkmate/finalPayment/FinalPaymentActivity.kt
+++ check-mate-app/app/src/main/java/com/example/checkmate/finalPayment/FinalPaymentActivity.kt
@@ -1,6 +1,7 @@
 package com.example.checkmate.finalPayment
 
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,26 +11,101 @@ import com.example.checkmate.data.model.ItemStatus
 import com.google.android.material.card.MaterialCardView
 import kotlinx.android.synthetic.main.activity_final_payment.*
 import android.graphics.*
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.example.checkmate.data.SessionState
 import com.example.checkmate.data.model.ItemPaymentDetails
 import com.example.checkmate.itemSelection.ItemSelectionViewModel
 import com.example.checkmate.itemSelection.ItemSelectionViewModelFactory
 
+class ColorToPaid(val color: String, val hasPaid: Boolean)
+
+class FinalPaymentAdapter(val context: Context, private val mData: List<ColorToPaid>) :
+    RecyclerView.Adapter<FinalPaymentAdapter.ViewHolder>() {
+    private lateinit var mInflater: LayoutInflater
+    private var mClickListener: ItemClickListener? = null
+
+
+    // inflates the row layout from xml when needed
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        this.mInflater = LayoutInflater.from(parent.context)
+
+        val view = mInflater.inflate(R.layout.final_payment_item, parent, false)
+
+
+        return ViewHolder(view)
+    }
+
+    // binds the data to the TextView in each row
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.myIconView.setImageDrawable(context.resources.getDrawable(R.drawable.icons8_checkmark_24))
+        holder.myColorView.setBackgroundColor(Color.GREEN)
+    }
+
+    // total number of rows
+    override fun getItemCount(): Int {
+        return mData.size
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+        internal var myIconView: ImageView
+
+        internal var myColorView: ImageView
+
+        init {
+            myIconView = itemView.findViewById(R.id.iconView)
+            myColorView = itemView.findViewById(R.id.colorView)
+
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(view: View) {
+            if (mClickListener != null) mClickListener!!.onItemClick(view, adapterPosition)
+        }
+    }
+
+    // convenience method for getting data at click position
+    internal fun getItem(id: Int): ColorToPaid {
+        return mData[id]
+    }
+
+    // allows clicks events to be caught
+    internal fun setClickListener(itemClickListener: ItemClickListener) {
+        this.mClickListener = itemClickListener
+    }
+
+    // parent activity will implement this method to respond to click events
+    interface ItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+}
 
 class FinalPaymentActivity : AppCompatActivity() {
 
     private lateinit var viewModel: ItemSelectionViewModel
+    private lateinit var listOfData: ArrayList<ColorToPaid>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_final_payment)
 
         val sessionState = intent.getParcelableExtra<SessionState>("sessionState")
-        viewModel = ViewModelProviders.of(this, ItemSelectionViewModelFactory(this.applicationContext))
+        viewModel = ViewModelProviders.of(this, ItemSelectionViewModelFactory(this))
             .get(ItemSelectionViewModel::class.java)
+        val myAdapter: FinalPaymentAdapter = FinalPaymentAdapter(this, listOfData)
+
+        listLayout.adapter = myAdapter;
+
         viewModel.poll(sessionState.billId.toString())
         poll()
+        listOfData = arrayListOf()
+
 
     }
 
@@ -40,26 +116,35 @@ class FinalPaymentActivity : AppCompatActivity() {
             colors.forEach { c ->
                 val view = createViewForColor(c, hasColorPaid(c, t.items.toList()))
 
-                linearLayout.addView(view)
+                //linearLayout.addView(view)
             }
 
-            val itemsList = arrayListOf<ItemStatus>(ItemStatus("lasagna", arrayOf(ItemPaymentDetails("#FFFFFF",1,true))),ItemStatus("pizza",
-                arrayOf(ItemPaymentDetails("#333333",1,false))))
+            val itemsList = arrayListOf<ItemStatus>(
+                ItemStatus("lasagna", arrayOf(ItemPaymentDetails("#FFFFFF", 1, true))), ItemStatus(
+                    "pizza",
+                    arrayOf(ItemPaymentDetails("#333333", 1, false))
+                )
+            )
 
 
         })
 
 
-        val itemsList = arrayListOf<ItemStatus>(ItemStatus("lasagna", arrayOf(ItemPaymentDetails("#FFFFFF",1,true))),ItemStatus("pizza",
-            arrayOf(ItemPaymentDetails("#333333",1,false))))
+        val itemsList = arrayListOf<ItemStatus>(
+            ItemStatus("lasagna", arrayOf(ItemPaymentDetails("#FFFFFF", 1, true))), ItemStatus(
+                "pizza",
+                arrayOf(ItemPaymentDetails("#333333", 1, false))
+            )
+        )
 
         val colors = getColors(itemsList)
 
-        colors.forEach { c ->
-            val view = createViewForColor(c, hasColorPaid(c,itemsList))
+        val newList = colors.map { c -> ColorToPaid(c, hasColorPaid(c, itemsList)) }
 
-            linearLayout.addView(view)
-        }
+        listOfData.addAll(newList)
+
+        listLayout.adapter!!.notifyDataSetChanged()
+
 
     }
 
@@ -74,7 +159,6 @@ class FinalPaymentActivity : AppCompatActivity() {
 
     private fun createViewForColor(color: String, hasPaidFor: Boolean): View {
         val card = MaterialCardView(this)
-
 
         val imageView = ImageView(this)
         if (hasPaidFor) {
@@ -94,14 +178,17 @@ class FinalPaymentActivity : AppCompatActivity() {
             }
 
             imageView.setImageDrawable(drawable)
-            card.addView(imageView)
+            //card.addView(imageView)
         }
 
 
         val bg = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(bg)
         val paint = Paint()
-        paint.color = Color.parseColor(color)
+        var r = Integer.valueOf(color!!.substring(1, 3), 16);
+        var g = Integer.valueOf(color!!.substring(3, 5), 16);
+        var b = Integer.valueOf(color!!.substring(5, 7), 16);
+        paint.color = Color.argb(100, r, g, b);
         canvas.drawRect(Rect(50, 10, 200, 200), paint)
 
         val imageViewForDrawable = ImageView(this)
