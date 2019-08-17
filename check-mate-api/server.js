064668ea2314d64dea8a652f6854f998909aaf04
Georgi Georgiev <georgiwess@gmail.com>
2019-08-17 14:07:58 +0300

    Increase payload limit

--- check-mate-api/server.js
+++ check-mate-api/server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(morgan(':method :url :response-time'));
 
 app.post('/new', async ({ body: { base64 = imageFromPathToBase64('./assets/bill.jpg') } }, res) => {
